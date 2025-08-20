797f05e3a28b2e94fc17b340e9feab6803d4e482
handikatriarlan <handikaarlan@gmail.com>
2025-08-20 19:13:59 +0700

    feat(expenses): add Filament expense resource

--- database/migrations/2025_08_18_035502_create_expense_items_table.php
+++ database/migrations/2025_08_18_035502_create_expense_items_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('expense_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('expensed_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('expenses_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->integer('qty');
             $table->integer('price');
